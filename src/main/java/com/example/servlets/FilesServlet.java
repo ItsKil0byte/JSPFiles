60eb99cad3018d5bdb3c2dc33af25453da341889
ItsKil0Byte <itskilobyte@gmail.com>
2025-02-26 22:25:44 +0500

    Make some basis for file system

--- src/main/java/com/example/servlets/FilesServlet.java
+++ src/main/java/com/example/servlets/FilesServlet.java
@@ -7,12 +7,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet("/files")
 public class FilesServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().print("<h1>File List</h1>");
+
+        String currentTime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+        req.setAttribute("time", currentTime);
+
+        Path path = Paths.get(req.getParameter("path"));
+        req.setAttribute("path", path);
+        req.setAttribute("parent", path.getParent());
+
+        req.getRequestDispatcher("files.jsp").forward(req, resp);
     }
 }
---
+++ src/main/webapp/files.jsp
@@ -0,0 +1,12 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>File list</title>
+</head>
+<body>
+    ${time}
+    <h1>${path}</h1>
+    <hr>
+    <a href="files?path=${parent}"> ⬅️ Back</a>
+</body>
+</html>
